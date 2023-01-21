b9d0f575d806ba46545fbe49a81516e6249a48f6
ustall <97186588+ustall@users.noreply.github.com>
2023-01-21 04:27:19 +0300

    Update popup.js

--- GHExtention/popup.js
+++ GHExtention/popup.js
@@ -1,10 +1,13 @@
+//popup.html buttons click event listner
 document.querySelector('.buttons').addEventListener('click', async (e) => {
+  
+  //text opening and splting
   const templates = await fetch('/templates.txt')
     .then(response => response.text())
     .then(text => text.split('\n'))
-    .then(lines => lines.map(line =>line.replace(/\\n/g,'\n')));
+    .then(lines => lines.map(line =>line.replace(/\\n/g,'\n')))
+  
+  //copy text to clipboard
   let textToCopy = templates[e.target.value];
   navigator.clipboard.writeText(textToCopy);
-  chrome.tabs.executeScript({
-    code: `document.activeElement.value = "${textToCopy}";`});
 });
